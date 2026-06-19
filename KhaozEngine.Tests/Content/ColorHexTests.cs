70849403a54567195de7796f719b8777d002e4b0
APKiwi <13660274+APKiwi@users.noreply.github.com>
2026-06-20 00:12:54 +1000

    content: hex color via Primitives.Color, delete ColorHex (breaking)

--- KhaozEngine.Tests/Content/ColorHexTests.cs
+++ KhaozEngine.Tests/Content/ColorHexTests.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Numerics;
-using KhaozEngine.Content;
+using KhaozEngine.Primitives;
 using Xunit;
 
 namespace KhaozEngine.Tests.Content
@@ -10,27 +9,27 @@ namespace KhaozEngine.Tests.Content
         [Fact]
         public void FromHex_ParsesRRGGBB_AsOpaque()
         {
-            var c = ColorHex.FromHex("#22D3EE");
-            Assert.Equal(0x22 / 255f, c.X, 4);
-            Assert.Equal(0xD3 / 255f, c.Y, 4);
-            Assert.Equal(0xEE / 255f, c.Z, 4);
-            Assert.Equal(1f, c.W, 4);
+            var c = Color.FromHex("#22D3EE");
+            Assert.Equal(0x22 / 255f, c.R, 4);
+            Assert.Equal(0xD3 / 255f, c.G, 4);
+            Assert.Equal(0xEE / 255f, c.B, 4);
+            Assert.Equal(1f, c.A, 4);
         }
 
         [Fact]
         public void FromHex_ParsesRRGGBBAA_Alpha()
         {
-            var c = ColorHex.FromHex("FF000080");
-            Assert.Equal(1f, c.X, 4);
-            Assert.Equal(0f, c.Y, 4);
-            Assert.Equal(0f, c.Z, 4);
-            Assert.Equal(0x80 / 255f, c.W, 4);
+            var c = Color.FromHex("FF000080");
+            Assert.Equal(1f, c.R, 4);
+            Assert.Equal(0f, c.G, 4);
+            Assert.Equal(0f, c.B, 4);
+            Assert.Equal(0x80 / 255f, c.A, 4);
         }
 
         [Fact]
         public void FromHex_HashIsOptional()
         {
-            Assert.Equal(ColorHex.FromHex("#10203040"), ColorHex.FromHex("10203040"));
+            Assert.Equal(Color.FromHex("#10203040"), Color.FromHex("10203040"));
         }
 
         [Theory]
@@ -39,20 +38,20 @@ namespace KhaozEngine.Tests.Content
         [InlineData("zzzzzz")]     // not hex
         public void FromHex_RejectsBadInput(string bad)
         {
-            Assert.ThrowsAny<Exception>(() => ColorHex.FromHex(bad));
+            Assert.ThrowsAny<Exception>(() => Color.FromHex(bad));
         }
 
         [Fact]
         public void ToHex_RoundTrips()
         {
             var original = "#22D3EEFF";
-            Assert.Equal(original, ColorHex.ToHex(ColorHex.FromHex(original)));
+            Assert.Equal(original, Color.ToHex(Color.FromHex(original)));
         }
 
         [Fact]
         public void ToHex_ClampsOutOfRange()
         {
-            Assert.Equal("#00FFFFFF", ColorHex.ToHex(new Vector4(-1f, 2f, 1f, 1f)));
+            Assert.Equal("#00FFFFFF", Color.ToHex(new Color(-1f, 2f, 1f, 1f)));
         }
     }
 }

853fd64c57deb7e7b10048dd44f27cb4c071bad7
APKiwi <anthropic2.ai@mail.ap.kiwi>
2026-09-08 15:17:27 +1000

    mapeditor(#368): reload clean documents from disk

--- KhaozEngine.MapEditor/MapEditorScene.cs
+++ KhaozEngine.MapEditor/MapEditorScene.cs
@@ -293,11 +293,7 @@ public partial class MapEditorScene : GameScene, IGameScene3D
         _controller.PlaceKind = DefaultPlaceKind();
         _controller.PlaceFeatureType = DefaultFeatureType();
 
-        _document.DocumentChanged += OnDocumentChanged;
-        _document.CommandApplied += OnCommandVisibilityForward;
-        _document.CommandRedone += OnCommandVisibilityForward;
-        _document.CommandUndone += OnCommandVisibilityInverse;
-        _document.Selection.Changed += OnSelectionChanged;
+        SubscribeDocument(_document);
 
         BuildWorld();
         RebuildOutline();
@@ -312,11 +308,7 @@ public partial class MapEditorScene : GameScene, IGameScene3D
     {
         if (!_built) return;
         _built = false;
-        _document.DocumentChanged -= OnDocumentChanged;
-        _document.CommandApplied -= OnCommandVisibilityForward;
-        _document.CommandRedone -= OnCommandVisibilityForward;
-        _document.CommandUndone -= OnCommandVisibilityInverse;
-        _document.Selection.Changed -= OnSelectionChanged;
+        UnsubscribeDocument(_document);
         TeardownWorld();
     }
 
